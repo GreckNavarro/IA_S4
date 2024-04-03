80b28d2e84d6ebbb59f620a69bb7c5ba22c417d5
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2024-04-03 17:19:25 -0500

    SensorIA4

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int hungry;
+    public int sleep;
+    public int wc;
+
+    public Transform AimOffset;
+}
